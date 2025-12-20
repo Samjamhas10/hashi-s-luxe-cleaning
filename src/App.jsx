e1e0b7cbe6763fef3ca77ff2d1e60a7ff3febca3
SamSam Hashi <sammymadobe@gmail.com>
2025-12-20 15:49:06 -0600

    Initialize Vite + React project with working dev server

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Hashi's Luxe Cleaning</h1>
+      <p>Your React + Vite app is now working!</p>
+    </div>
+  );
+}
+
+export default App;
