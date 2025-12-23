e23ea0e80f7562c9d4db6c8fc63693961ec3f161
SamSam Hashi <sammymadobe@gmail.com>
2025-12-22 23:14:41 -0600

    Add initial component scaffolding

---
+++ src/components/App/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// import components
+import Home from "../Home/Home";
+import Navigation from "../Navigation/Navigation";
+import Services from "../Services/Services";
+import Pricing from "../Pricing/Pricing";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+
+// import styles
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Navigation />
+      <Services />
+      <Pricing />
+      <Contact />
+      <Footer />
+      <h1>Welcome to Hashi's Luxe Cleaning!</h1>
+    </div>
+  );
+}
+
+export default App;
